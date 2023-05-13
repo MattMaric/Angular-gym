6cd6e80cc181b9571e9fed23f0a5e7756b93b17a
MattMaric <101636994+MattMaric@users.noreply.github.com>
2023-05-13 18:25:44 +0200

    upload internship project part 3

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  // firebase : {
+  //   apiKey: "AIzaSyBpYZcR_tOMLBodvP557N4sfL2aCWTKPxo",
+  //   authDomain: "angulargym-dcf99.firebaseapp.com",
+  //   databaseURL: "https://angulargym-dcf99-default-rtdb.firebaseio.com",
+  //   projectId: "angulargym-dcf99",
+  //   storageBucket: "angulargym-dcf99.appspot.com",
+  //   messagingSenderId: "71644151242",
+  //   appId: "1:71644151242:web:3785381fb1fe12048ae333",
+  //   measurementId: "G-MHSMRPE7SB"
+  // }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+//import { getDatabase } from "@angular/fire/database"
+
+export const environment = {
+  production : false,
+  firebase : {
+    apiKey: "AIzaSyBpYZcR_tOMLBodvP557N4sfL2aCWTKPxo",
+    authDomain: "angulargym-dcf99.firebaseapp.com",
+    databaseURL: "https://angulargym-dcf99-default-rtdb.firebaseio.com",
+    projectId: "angulargym-dcf99",
+    storageBucket: "angulargym-dcf99.appspot.com",
+    messagingSenderId: "71644151242",
+    appId: "1:71644151242:web:3785381fb1fe12048ae333",
+    measurementId: "G-MHSMRPE7SB"
+  }
+};
+//const db = getDatabase();
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
